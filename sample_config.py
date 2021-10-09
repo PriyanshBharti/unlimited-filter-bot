e5802f9bff7001d4b2fbafd75c71ef618c3ef1a8
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-09 17:20:39 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
+import time
+
+class Config(object):
+
+    # Get a bot token from botfather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+
+    # Get from my.telegram.org
+    API_ID = int(os.environ.get("API_ID", 12345))
+
+
+    # Get from my.telegram.org
+    API_HASH = os.environ.get("API_HASH", "")
+    
+    
+    # Database URL from https://cloud.mongodb.com/
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+
+
+    # Your database name from mongoDB
+    DATABASE_NAME = str(os.environ.get("DATABASE_NAME", "Cluster0"))
+
+
+    # ID of users that can use the bot commands
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+
+    # To save user details (Usefull for getting userinfo and total user counts)
+    # May reduce filter capacity :(
+    # Give yes or no
+    SAVE_USER = os.environ.get("SAVE_USER", "no").lower()
+
+
+    # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
+    # To check dyno status
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
+
+
+    # OPTIONAL - To set alternate BOT COMMANDS
+    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
+    DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "del")
+    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delall")
+    CONNECT_COMMAND = os.environ.get("CONNECT_COMMANDD", "connect")
+    DISCONNECT_COMMAND = os.environ.get("DISCONNECT_COMMANDD", "disconnect")
+
+
+    # To record start time of bot
+    BOT_START_TIME = time.time()
