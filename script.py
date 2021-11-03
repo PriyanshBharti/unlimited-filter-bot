c46b3f03b3e5cf728977631e7dbd11a718c08c5f
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-03 21:16:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -16,32 +16,48 @@ class Script(object):
 
     START_MSG = """<b>Hi {},
 
-Kei hi ZoSeries Studio Bot ka ni e.</b>
-A hnuaia Group & Channel te hi Join Rawh :)
+I'm an advanced filter bot with many capabilities!</b>
+There is no practical limits for my filtering capacity :)
 
-<b>Send <i>/help</i> for more details.</b>
+<b>See <i>/help</i> for commands and more details.</b>
 """
 
 
     HELP_MSG = """
-<b>No one gonna help you 🤣🤣</b>
+<b>Commands Help</b>
 
+Add me as admin in your group and start filtering :)
+
+<b><u>Basic Commands:</u></b>
+/start - Check if I'm alive!
+/help - Command help
+/about - Something about me!
+
+<b><u>Filter Commands:</u></b>
+/add <code>name reply</code>: Add filt.er for name.
+/del <code>name</code>: Delete filter.
+/delall: Delete entire filters (Group Owner Only!).
+/viewfilters: List all filters in chat.
+
+<b><u>Connection Commands:</u></b>
+/connect <code>groupid</code>: Connect your group to my PM. You can also simply use.
+/connect: Only in groups.
+/connections: Manage your connections.
+
+<b><u>Extras:</u></b>
+/status: Shows current status of your bot (Auth User Only).
 /id: Shows ID information.
-/info: Shows User Information.
-
-How to Get File : <a href='https://t.me/mizotginfotel/37'>Press me 🥰</a> 
+/info: Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 <b>© Developed by ❤️ @ZauteKm</b>
 """
 
 
-    ABOUT_MSG = """🤖<b>My Name: ZoSeriesBot</b>
+    ABOUT_MSG = """🤖<b>My Name: Filter Bot</b>
 
 👨‍💻<b>Creator:</b> @ZauteKm    
 
 ♻️<b>Language:</b> <code>Python3</code>
-
-👥<b>Group: @MizoTelegram
 
 📜<b>Library:</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
