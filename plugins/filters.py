12c035ad13afabed92364e1fab01b24e28b30d69
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-13 08:00:07 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -342,8 +342,8 @@ async def give_filter(client,message):
             reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
 
             if reply_text:
-                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
-                reply_to_message_id=message.message_id
+                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t"),
+                reply_to_message_id=message.reply_to_message.message_id
 
             if btn is not None:
                 try:
@@ -354,7 +354,7 @@ async def give_filter(client,message):
                             button = eval(btn)
                             await message.reply_text(
                                 reply_text,
-                                reply_to_message_id=message.message_id,
+                                reply_to_message_id=message.reply_to_message.message_id,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button)
                             )
